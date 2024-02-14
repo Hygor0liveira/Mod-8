35b06cf5a9e155cd9d536ff6488f4db125a6394e
Hygor0liveira <hygorbarreto51@gmail.com>
2024-02-13 22:52:35 -0300

    first commit

---
+++ src/Ebac.java
@@ -0,0 +1,17 @@
+public class Ebac {
+    public static void main(String args []) {
+        int num1 = 10;
+        int num2 = 15;
+        int num3 = 20;
+        
+        int num4 = num2  + num3;
+        int num5 = num3 / num1;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num3);
+        System.out.println(num3 * num5);
+        System.out.println((num5 + num4) / num1);
+        System.out.println(num5 / num1);
+        System.out.println(num5 + num4);
+        System.out.println((num3 + num1) / 2);
+    }
+}
